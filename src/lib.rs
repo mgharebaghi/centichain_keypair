22d0794189880ecb6000a919eedd429f0201a658
Mohammad <mgharebaghi92@gmail.com>
2025-01-05 14:18:31 +0330

    change to from phrase in checks

--- src/lib.rs
+++ src/lib.rs
@@ -22,9 +22,9 @@ impl CentichainKey {
     ///Returning the public key by getting the seed phrase
     /// ````
     pub fn check_phrase<'a>(seed_phrase: &String) -> Result<Public, &'a str> {
-        let keypair = ed25519::Pair::from_string(seed_phrase, None);
+        let keypair = ed25519::Pair::from_phrase(seed_phrase, None);
         match keypair {
-            Ok(pair) => Ok(pair.public()),
+            Ok(pair) => Ok(pair.0.public()),
             Err(_) => Err("Your seed phrase is wrong!"),
         }
     }
@@ -32,9 +32,9 @@ impl CentichainKey {
     ///Taking input and returning a signature accepted by the Centichain network
     /// ````
     pub fn signing<'a>(seed_phrase: &String, message: &String) -> Result<Signature, &'a str> {
-        let keypair = ed25519::Pair::from_string(&seed_phrase, None);
+        let keypair = ed25519::Pair::from_phrase(&seed_phrase, None);
         match keypair {
-            Ok(pair) => Ok(pair.sign(message.as_bytes())),
+            Ok(pair) => Ok(pair.0.sign(message.as_bytes())),
             Err(_) => Err("Your seed phrase is wrong!"),
         }
     }
